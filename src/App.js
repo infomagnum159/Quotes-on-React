2d8ecfd011717daae0ca5f29b965389af17d31f3
infomagnum159 <infomagnum159@gmail.com>
2022-07-09 18:41:05 +0600

    Added routes to every links

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Route, Switch, BrowserRouter} from 'react-router-dom';
+import Quotes from "./containers/Quotes/Quotes";
+import SubmitNewQuote from "./containers/SubmitNewQuote/SubmitNewQuote";
+import EditQuote from "./containers/EditQuote/EditQuote";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" exact component={Quotes}/>
+            <Route path="/add-quote" component={SubmitNewQuote}/>
+            <Route path="/quotes/:category" component={Quotes}/>
+            <Route path="/quote/:id/edit" component={EditQuote}/>
+            <Route render={() => <h1>404 Not Found</h1>}/>
+        </Switch>
+    </BrowserRouter>
+);
 
 export default App;
