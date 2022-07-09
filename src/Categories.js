7a22e6810236ca7196e02e0d17697594b963ff0e
infomagnum159 <infomagnum159@gmail.com>
2022-07-09 17:26:40 +0600

    Added categories

---
+++ src/Categories.js
@@ -0,0 +1,8 @@
+
+export const CATEGORY = [
+    {title: 'Famous people', category: 'famous-people'},
+    {title: 'Humour', category: 'humour'},
+    {title: 'Saying', category: 'saying'},
+    {title: 'Motivational', category: 'motivational'},
+    {title: 'Star Wars', category: 'star-wars'}
+];
